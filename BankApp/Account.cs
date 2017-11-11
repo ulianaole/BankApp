08a2991056a695d7b693b962c5409569bc3b436d
uliana <uliana@DESKTOP-I6A41N0>
2017-11-11 12:18:16 -0800

    Initial comment bank app

---
+++ BankApp/Account.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankApp
+{
+    /// <summary>
+    /// This is about a bank
+    /// account
+    /// </summary>
+    class Account
+    {
+        #region Properties
+        //this is a single line comment
+        /*
+         * 
+         * 
+         * */
+        
+        public int AccountNumber { get; set; }
+        public string EmailAddress { get; set; }
+        public string AccountName { get; set; }
+        public string AccountType { get; set; }
+        public decimal Balance { get; set; }
+        #endregion
+
+    }
+}
