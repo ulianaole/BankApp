fe31b6e1b9251e12e5b2d371f0c348ce08b3faf2
uliana <uliana@DESKTOP-I6A41N0>
2017-12-09 12:56:16 -0800

    Loops and linq

---
+++ BankApp/Bank.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankApp
+{
+    static class Bank
+    {
+        private static List<Account> accounts = new List<Account>();
+
+        public static Account CreateAccount(string emailAddress, 
+            string accountName = "Default Account",
+            TypeOfAccount accountType = TypeOfAccount.Checking)
+        {
+            var account = new Account
+            {
+                EmailAddress = emailAddress,
+                AccountName = accountName,
+                AccountType = accountType
+            };
+            accounts.Add(account);
+            return account;
+        }
+        public static List<Account> GetAllAccounts()
+        {
+            return accounts;
+        }
+
+        public static void Deposit(int accountNumber, decimal amount)
+        {
+           var account = accounts.Where(a => a.AccountNumber == accountNumber).FirstOrDefault();
+           if (account != null)
+            {
+                account.Deposit(amount);
+            }
+        }
+    }
+}
--- BankApp/Program.cs
+++ BankApp/Program.cs
@@ -10,32 +10,72 @@ namespace BankApp
     {
         static void Main(string[] args)
         {
-            // Comment: reference, object, instance of a class
-            var account = new Account
-            {
-                AccountName = "My checking",
-                AccountType = TypeOfAccount.Checking,
-                EmailAddress = "test@test.com"
-            };
-
-
-            account.Deposit(100.10M);
-            Console.WriteLine($"Number:{account.AccountNumber}, EmailAddress:{account.EmailAddress}, Blanace:{account.Balance}, AccountType{account.AccountType}");
-
-            var account2 = new Account
-            {
-                AccountName = "My savings",
-                EmailAddress = "test2@test.com",
-                AccountType = TypeOfAccount.Savings
-            };
            
+            Console.WriteLine("*******************************************");
+            Console.WriteLine("Welcome to my bank");
+            Console.WriteLine("*******************************************");
+            while (true)
+            {
+                Console.WriteLine("0. Exit");
+                Console.WriteLine("1. Create an account");
+                Console.WriteLine("2. Deposit");
+                Console.WriteLine("3. Withdraw");
+                Console.WriteLine("4. Print all accounts");
 
-            account2.Deposit(100.10M);
-            Console.WriteLine($"Number:{account2.AccountNumber}, EmailAddress:{account2.EmailAddress}, Blanace:{account2.Balance}, AccountType{account2.AccountType}");
+                Console.Write("Please choose one option from above");
+                var choice = Console.ReadLine();
+
+                switch (choice)
+                {
+                    case "0":
+                        Console.WriteLine("Thank you for visiting");
+                        return;
+                    case "1":
+                        Console.Write("Email Address: ");
+                        var emailAddress = Console.ReadLine();
+                        Console.Write("Account name: ");
+                        var accountName = Console.ReadLine();
+                        var typeOfAccounts = Enum.GetNames(typeof(TypeOfAccount));
+                        for (var i = 0; i < typeOfAccounts.Length; i++)
+                        {
+                            Console.WriteLine($"{i + 1}.{typeOfAccounts[i]}");
+                        }
+                        Console.Write("type of account: ");
+                        var accountType = Convert.ToInt32(Console.ReadLine());
+                        var account = Bank.CreateAccount(emailAddress, accountName, (TypeOfAccount)(accountType - 1));
+                        Console.WriteLine($"AN:{account.AccountNumber}, Balance:{account.Balance}, TA: {account.AccountType}");
+                        break;
+                    case "2":
+                        PrintAllAccounts();
+                        Console.Write("Account Number:  ");
+                        var an = Convert.ToInt32(Console.ReadLine());
+                        Console.Write("Amount to deposit: ");
+                        var amount = Convert.ToDecimal(Console.ReadLine());
+                        Bank.Deposit(an, amount);
+                        break;
+                    case "3":
+                        break;
+                    case "4":
+                        PrintAllAccounts();
+
+                        break;
+                    default:
+                        Console.WriteLine("Invalid choice! Please try again!");
+                        break;
+                }
+
+            }
 
         }
 
+        private static void PrintAllAccounts()
+        {
+            var accounts = Bank.GetAllAccounts();
+            foreach (var acnt in accounts)
+            {
+                Console.WriteLine($"AN:{acnt.AccountNumber}, Balance:{acnt.Balance}, TA: {acnt.AccountType}");
 
-
+            }
+        }
     }
 }
